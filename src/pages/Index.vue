f4c295706636b46955ceb4c5d784b07d689bbb75
Hamilton Mendes <amilzito@gmail.com>
2022-06-08 17:37:01 -0300

    Squashed 'subtreeDirectory/' content from commit e506bc1
    git-subtree-dir: subtreeDirectory
    git-subtree-split: e506bc16b7c54f1e3101ca2fd1e36886c47ba86c

---
+++ src/pages/Index.vue
@@ -0,0 +1,21 @@
+<template>
+    <h3>Star Wars Quasar SWAPI App</h3>
+    <p>
+        This is a demo application that I decided to experiment with Quasar
+        (https://quasar.dev/) using their CLI to create a Vue application with
+        Typescript, and the SWAPI API (https://swapi.dev/) as a data provider. A
+        list of problems I encountered and possible improvements to the
+        application will follow.
+    </p>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+
+export default defineComponent({
+    name: 'PageIndex',
+    setup() {
+        return;
+    },
+});
+</script>
