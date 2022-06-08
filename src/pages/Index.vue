e506bc16b7c54f1e3101ca2fd1e36886c47ba86c
Hamilton Mendes <amilzito@gmail.com>
2022-06-08 17:36:49 -0300

    Modified readme and introduction.

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,49 +1,21 @@
 <template>
-  <q-page class="row items-center justify-evenly">
-    <example-component
-      title="Example component"
-      active
-      :todos="todos"
-      :meta="meta"
-    ></example-component>
-  </q-page>
+    <h3>Star Wars Quasar SWAPI App</h3>
+    <p>
+        This is a demo application that I decided to experiment with Quasar
+        (https://quasar.dev/) using their CLI to create a Vue application with
+        Typescript, and the SWAPI API (https://swapi.dev/) as a data provider. A
+        list of problems I encountered and possible improvements to the
+        application will follow.
+    </p>
 </template>
 
 <script lang="ts">
-import { Todo, Meta } from 'components/models';
-import ExampleComponent from 'components/CompositionComponent.vue';
 import { defineComponent, ref } from 'vue';
 
 export default defineComponent({
-  name: 'PageIndex',
-  components: { ExampleComponent },
-  setup() {
-    const todos = ref<Todo[]>([
-      {
-        id: 1,
-        content: 'ct1'
-      },
-      {
-        id: 2,
-        content: 'ct2'
-      },
-      {
-        id: 3,
-        content: 'ct3'
-      },
-      {
-        id: 4,
-        content: 'ct4'
-      },
-      {
-        id: 5,
-        content: 'ct5'
-      }
-    ]);
-    const meta = ref<Meta>({
-      totalCount: 1200
-    });
-    return { todos, meta };
-  }
+    name: 'PageIndex',
+    setup() {
+        return;
+    },
 });
 </script>
